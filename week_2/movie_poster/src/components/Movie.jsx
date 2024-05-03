7af58d8372147016d8baa3c9cf5110385b32d155
OhDG <ohdonggyu@naver.com>
2024-05-03 15:49:17 +0900

    [feat[ Week2 Mission2 Movie Poster

---
+++ week_2/movie_poster/src/components/Movie.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Detail from "./Detail";
+
+export default function Movie({ poster, title, vote_average, overview }) {
+    // 네 가지 props를 받음
+    return (
+        <div className="movie">
+            <img src={`https://image.tmdb.org/t/p/w200${poster}`} />
+            <div className="movieInfo">
+                <div className="movieTitle">{title}</div>
+                <div className="vote_avg">{vote_average}</div>
+            </div>
+            <Detail title={title} overview={overview} />
+        </div>
+    );
+}
