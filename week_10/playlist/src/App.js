ed70037c9559a2f5ccc074cafb79b47a7b1edb1e
OhDG <ohdonggyu@naver.com>
2024-06-29 04:53:11 +0900

    [feat] 10주차 playlist 미션 완료

---
+++ week_10/playlist/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./store";
+import Cart from "./Cart";
+
+const App = () => (
+    <Provider store={store}>
+        <Cart />
+    </Provider>
+);
+
+export default App;
